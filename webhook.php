453f332b63c7e59e8000d8cde96caa5eda1b08c8
Gesiel <gesiel@srv20109>
2018-08-02 14:35:00 +0000

    Fix errors

--- webhook.php
+++ webhook.php
@@ -1,20 +1,18 @@
 <?php
-
 class GKdata{
         public $hashid;
         public $key;
         public $iv;
 }
-
 function getKey() {
         /*You need load hashid,key and iv. If you save in the database, you need find us in this function*/
         $GK = new GKdata;
-        $GK->hashid = 'asdasdasda';
-        $GK->key = 'asd';
-        $GK->iv = 'dsa';
+        $GK->hashid = '';
+        $GK->key = '';
+        $GK->iv = '';
         return $GK;
-
 }
+
 
 function doAction($data) {
 	$GKdata = json_decode($data);
@@ -37,21 +35,30 @@ function doAction($data) {
 	
 			*/
 }		
-			
 
+
+			
 if ($_POST) {
+	
 		$GKinfo = getKey();
+		
         if ($_POST['hashid'] == $GKinfo->hashid ) {
 			$key = base64_decode($GKinfo->key);
 			$iv  = base64_decode($GKinfo->iv);
 			try {
-				$output = openssl_decrypt(base64_decode($_POST['data'])	, 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv);
-			} catch (Exception $e) {
+				$msgcrypt = base64_decode($_POST['data']);
+				$output = openssl_decrypt($msgcrypt, 'aes-256-cfb8', $key, 1, $iv);
+			    }
+			 catch (Exception $e) {
 				echo 'Error decrypting: ',  $e->getMessage(), "\n";
 			}
+			
 			if ($output) {
 				doAction($output);
+				
 			}
-		}
+		
+	}
 }
+
 ?>
