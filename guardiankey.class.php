68528ddb4ccd1c3f05f9b3f481f61b4e66b719d4
Gesiel <gesiel@srv20109>
2018-09-22 19:24:15 +0000

    Fix errors

--- guardiankey.class.php
+++ guardiankey.class.php
@@ -1,7 +1,5 @@
 <?php
-
 // Please run "register.php" for generate your configuration
-
 $GKconfig = array(
 	'email' => "",
 	'hashid' => "",
@@ -13,11 +11,18 @@ $GKconfig = array(
 	'reverse' => "",
 );
 
-
+function convert_before_json(&$item, $key) {
+         $item = utf8_encode($item);
+}
 
 class guardiankey {
-     
-    function send_event($username)  {
+
+function _json_encode($obj) {
+        array_walk_recursive($obj, "convert_before_json");
+        return json_encode($obj,JSON_UNESCAPED_SLASHES);
+}
+
+	function create_message($username) {
 		global $GKconfig;
 		
         $keyb64    = $GKconfig['key'];
@@ -28,9 +33,7 @@ class guardiankey {
         $authgroupid    = $GKconfig['groupid'];
         $reverse   = $GKconfig['reverse'];
         $timestamp = time();
-
         if(strlen($hashid)>0){
-
           $key=base64_decode($keyb64);
           $iv=base64_decode($ivb64);
           
@@ -48,52 +51,28 @@ class guardiankey {
           $json->userAgent=substr($_SERVER['HTTP_USER_AGENT'],0,500);
           $json->psychometricTyped="";
           $json->psychometricImage="";
-     
-          $message = json_encode($json,JSON_UNESCAPED_SLASHES);
+          $tmpmessage = $this->_json_encode($json);
+		  $blocksize=8;
+          $padsize = $blocksize - (strlen($tmpmessage) % $blocksize);
+          $message=str_pad($tmpmessage,$padsize," ");
+		  $cipher = openssl_encrypt($message, 'aes-256-cfb8', $key, 0, $iv);
+		  return $cipher;
+		}
+	}
+    function send_event($username)  {
+	
           // some PHP versions needs it.. aff
-          $cipher = openssl_encrypt($message, 'aes-256-cfb8', $key, 0, $iv);
+		  $cipher = create_message($username);
           $payload=$hashid."|".$cipher;
           $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
           socket_sendto($socket, $payload, strlen($payload), 0, "collector.guardiankey.net", "8888");
-        }
+        
     }
     
     function check_access($username) {
-		
-			global $GKconfig;
 		$guardianKeyWS='https://api.guardiankey.io/checkaccess';
-		
-        $keyb64    = $GKconfig['key'];
-        $salt      = $GKconfig['Salt'];
-        $ivb64 	   = $GKconfig['iv'];
-        $hashid    = $GKconfig['id'];
-        $orgid     = $GKconfig['orgid'];
-        $authgroupid    = $GKconfig['groupid'];
-        $reverse   = $GKconfig['reverse'];
-        $timestamp = time();
-
-        if(strlen($hashid)>0){
-
-          $key=base64_decode($keyb64);
-          $iv=base64_decode($ivb64);
-          
-          $json = new stdClass();
-          $json->generatedTime=$timestamp;
-          $json->agentId=$hashid;
-          $json->organizationId=$hashid;
-          $json->authGroupId=$hashid;
-          $json->service=$GKconfig['service'];;
-          $json->clientIP=$_SERVER['REMOTE_ADDR'];
-          $json->clientReverse = ($reverse==1)?  gethostbyaddr($json->clientIP) : "";
-          $json->userName=$username;
-          $json->authMethod="";
-          $json->loginFailed="0";
-          $json->userAgent=substr($_SERVER['HTTP_USER_AGENT'],0,500);
-          $json->psychometricTyped="";
-          $json->psychometricImage="";
-     
-          $message = base64_encode(json_encode($json,JSON_UNESCAPED_SLASHES));
-          $data = array(
+		$message = create_message($username);
+        $data = array(
 					'hashid' => $hashid,
 					'message' => $message
 					);
