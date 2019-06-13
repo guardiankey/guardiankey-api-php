e7ad8a3ad2e539dfba47d1537e13d8af6ba88a42
Gesiel <gesiel@srv20109>
2019-06-13 12:50:27 +0000

    save

--- webhook.php
+++ webhook.php
@@ -8,27 +8,28 @@ function doAction($data) {
 	/*In this function do you need define what action the webhook
 	will make. Example:
 		- Alert by email the users, and the username is the email of the user:
-	
-	$subject = "Atypical login from ".$GKdata->location;
+
+	$GK = new guardiankey($GKconfig);	
+	$subject = "Atypical login from ".$data['city'].'/'.$data['region'].'/'.$data['country'];
 	$message = "Hi,
 	
 			We detectead a atypical login on your account. Please view below:
-			Device:".GKdata->system."
-			IP:".$GKdata->ipaddress."
-			Location: ".$GKdata->location."
+			Device:".$data['cliente_ua'].'/'.$data['client_os']."
+			IP:".$data['clientIP']."
+			Location: ".$data['city'].'/'.$data['region'].'/'.$data['country']."
 			
 			Please confirm in link below if are you or not:
-			".$GKconfig['resolveEventURL']."?t=".$GKdata['EVENTTOKEN']."&e=".$GKdata['EVENTID'].";
+			".$GKconfig['resolveEventURL']."?t=".$data['event_token']."&e=".$data['eventId'];
 			
-	mail($GKdata->username,$subject,$message);
+	mail($data->userName,$subject,$message);
 	
 			*/
 }		
 
 
-			
-if ($_POST) {
-	
+$data = json_decode(file_get_contents('php://input'), true);
+if ($data) {	
+
     try {
         $GK = new guardiankey($GKconfig);
         $output=$GK->processWebHookPost($authgroupid,$keyb64,$ivb64);
